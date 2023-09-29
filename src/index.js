39de3506c773bdf8f3954678470a635e68ead896
Antonio <abamorim36@gmail.com>
2023-09-29 07:11:42 -0300

    Requirement: Update Todo

--- src/index.js
+++ src/index.js
@@ -77,29 +77,41 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   
+  const { user } = request;
   const { title, deadline } = request.body;
   const { id } = request.params;
-  
-  // Checks if Todo exists by id
-  if()  {
-    return response.status(404).json({error: "Todo Not Exist!"});
+
+  const todo = user.todos.find((todo) =>
+    todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found" })
   }
 
-  // Method: Find the todo object by id
-  // Update the title and deadline
+  todo.title = title;
+  todo.deadline = new Date(deadline);
 
-  return response.status(201);
+  return response.status(201).json(todo);
 
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find((todo) =>
+    todo.id === id);
+
+  todo.done = true;
+
+  return response.json(todo);
+  
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.listen(3333);
+// app.listen(3333);
 
 module.exports = app;
